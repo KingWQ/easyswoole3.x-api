32179541585ca95e30842fa3f8d6fa28dece523f
wysmacmini02 <2971749820@qq.com>
2019-08-21 18:57:12 +0800

    update

---
+++ App/HttpController/Test/TaskTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wysmacmini02
+ * Date: 2019/8/21
+ * Time: 18:53
+ */
+namespace App\HttpController\Test;
+
+use EasySwoole\Http\AbstractInterface\Controller;
+
+class TaskTest extends Controller
+{
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+
+    public function demo()
+    {
+
+    }
+}
